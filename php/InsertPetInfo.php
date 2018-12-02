419bb1f45592a97409c6949ab8df43ca8d928356
大吾郎 <saxon773@yahoo.co.jp>
2018-12-02 23:27:43 +0900

    ぺット情報登録画面作成：PHP

---
+++ php/InsertPetInfo.php
@@ -0,0 +1,95 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+<meta charset="utf-8">
+<title>ペット情報</title>
+<link rel="stylesheet" href="../css/Element.css">
+<link rel="stylesheet" href="../css/pet.css">
+<script src="../js/InsertPetInfo.js"></script>
+</head>
+<body>
+    <header>
+        <h1>ペット一覧</h1>
+    </header>
+    <aside>
+        <ul>
+            <li><a href="Select.php">ペット一覧</a></li>
+            <li><a href="InsertPetInfo.php">ペット情報登録</a></li>
+        </ul>
+    </aside>
+    <form method_exists="post" action="InsertPetInfo.php" >
+        <article>
+            <h2>
+                ぺット情報登録
+            </h2>
+            <table class="insertPetInfoTable">
+                <colgroup width="150"></colgroup>
+                <colgroup width="300"></colgroup>
+                <tr>
+                    <th>
+                        ぺット名
+                    </th>
+                    <td>
+                        <input type="text" id="pet_name" />
+                    </td>
+                <tr>
+                    <th>
+                        誕生日
+                    </th>
+                    <td>
+                        <input type="date" id="birthday" />
+                    </td>
+                </tr>
+                <tr>
+                    <th>
+                        品種
+                    </th>
+                    <td>
+                        <input type="text" id="pet_type" />
+                    </td>
+                </tr>
+                <tr>
+                    <th>
+                        カラー
+                    </th>
+                    <td>
+                        <input type="text" id="color" />
+                    </td>
+                </tr>
+                <tr>
+                    <th>
+                        備考
+                    </th>
+                    <td>
+                        <textarea id="remarks"></textarea>
+                    </td>
+                </tr>
+                <tr>
+                    <th>
+                        画像
+                    </th>
+                    <td>
+                        <input type="file" id="image_path" name="file" />
+                    </td>
+                </tr>
+            </table>
+            <div class="center">
+                <input type="submit" id="insert" value="登録" onclick="checkMessage();"/>
+            </div>
+        </article>
+    </form>
+    <?php
+        if(!isset($_POST["pet_name"])){
+            exit;
+        }
+         $pet_name = $_POST["pet_name"];
+         $birthday = $_POST["birthday"];
+         $pet_type = $_POST["pet_type"];
+         $color = $_POST["color"];
+         $remarks = $_POST["remarks"];
+         $image_path = $_POST["image_path"];
+
+         echo "<script>alert('テスト')</script>";
+    ?>
+</body>
+</html>
