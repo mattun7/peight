744854272eaf8b467db85a45c510f66a95339b7a
大吾郎 <saxon773@yahoo.co.jp>
2018-12-09 22:35:28 +0900

    PDOの共通化

---
+++ php/Util/DbConnection.php
@@ -0,0 +1,20 @@
+<?php
+
+class DbConnection {
+    public static $pdo = NULL;
+
+    public static function get_connection(){
+        if(is_null(self::$pdo)){
+            $dsn = 'mysql:dbname=PetWeightInfo;host=localhost;charset=utf8mb4';
+            $username = 'root';
+            $password = '';
+            try{
+                self::$pdo = new PDO($dsn, $username, $password);
+            } catch (Exception $e) {
+                throw new Exception($e->getMessage());
+            }
+        }
+        return self::$pdo;
+    }
+}
+?>
