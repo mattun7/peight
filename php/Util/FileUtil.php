ea82cced69dcb4e02182eee785b5837ab0f1f1f7
大吾郎 <saxon773@yahoo.co.jp>
2018-12-09 22:38:46 +0900

    ファイル送信共通化

---
+++ php/Util/FileUtil.php
@@ -0,0 +1,14 @@
+<?php
+class FileUtil{
+    public static function imageUpload($image_path){
+
+        if(empty($image_path)){
+            return;
+        }
+        $tmp_name = $_FILES['pet_image']['tmp_name'];
+        $result = move_uploaded_file($tmp_name, $image_path);
+
+        return $result;
+    }
+}
+?>
