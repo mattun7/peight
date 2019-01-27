2b8cdcc5668804b41b9c4ae11358e8c98fcc2fdb
大吾郎 <saxon773@yahoo.co.jp>
2019-01-27 21:37:09 +0900

    クラス名変更

--- php/Exception/WebAPIException.php
+++ php/Exception/WebAPIException.php
@@ -1,5 +1,5 @@
 <?php
-class ErrorException extends Exception{
+class WebAPIException extends Exception{
     public function __construct($message) {
         error_log($message . '\n');
         header("HTTP/1.0 404 Not Found");
