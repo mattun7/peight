ac4f4df9715caca35d887bffd01bc3f13de22182
大吾郎 <saxon773@yahoo.co.jp>
2019-03-06 22:43:43 +0900

    DBの情報を画面に表示

--- index.php
+++ index.php
@@ -27,6 +27,10 @@
 			</div>
 		</div>
 	</nav>
-	<?php echo parse_url(getenv('DATABASE_URL')); ?>
+	<?php 
+	echo getenv('DATABASE_URL');
+	$url = parse_url(getenv('DATABASE_URL'));
+	if(empty($url['path']))echo $url['path'];
+	?>
 </body>
 </html>
